0928254294e5317a70335c4b419de573965ef5ef
AUSoftAndreas <andreas_hoefer@cdemy.de>
2022-04-11 11:23:53 +0200

    first commit

---
+++ bin/itemis.dart
@@ -0,0 +1,5 @@
+import 'package:itemis/itemis.dart' as itemis;
+
+void main(List<String> arguments) {
+  print('Hello world: ${itemis.calculate()}!');
+}
---
+++ test/itemis_test.dart
@@ -0,0 +1,8 @@
+import 'package:itemis/itemis.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
