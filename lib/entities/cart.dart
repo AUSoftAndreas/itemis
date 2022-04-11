3a83a06d7eb7128f563953be330da6c4ee3799b4
AUSoftAndreas <andreas_hoefer@cdemy.de>
2022-04-11 14:45:56 +0200

    cart completed

--- lib/entities/cart.dart
+++ lib/entities/cart.dart
@@ -4,6 +4,14 @@ import 'package:itemis/entities/product.dart';
 class Cart {
   List<CartItem> items = [];
 
+  get priceNet => items.fold<int>(0, (previousValue, element) => previousValue + element.priceNet);
+
+  get vatValue => items.fold<int>(0, (previousValue, element) => previousValue + element.vatValue);
+
+  get importDutyValue => items.fold<int>(0, (previousValue, element) => previousValue + element.importDutyValue);
+
+  get priceGross => items.fold<int>(0, (previousValue, element) => previousValue + element.priceGross);
+
   void add(CartItem cartItem) {
     items.add(cartItem);
   }
--- test/cart_test.dart
+++ test/cart_test.dart
@@ -1,6 +1,8 @@
 import 'package:itemis/entities/cart.dart';
 import 'package:itemis/entities/cart_item.dart';
+import 'package:itemis/entities/origin.dart';
 import 'package:itemis/entities/product.dart';
+import 'package:itemis/entities/vat.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -26,5 +28,29 @@ void main() {
     expect(cart.items.length, 1);
     cart.remove(cartItem);
     expect(cart.items.length, 0);
+  });
+
+  test('Combined case', () {
+    final cart = Cart();
+    final productA = Product(priceNet: 1000000, vat: FullVat(), origin: DomesticOrigin());
+    final productB = Product(priceNet: 2000000, vat: ReducedVat(), origin: InternationalOrigin());
+    final cartItemA = CartItem(product: productA, quantity: 2);
+    final cartItemB = CartItem(product: productB, quantity: 3);
+    expect(cart.items.length, 0);
+    cart.add(cartItemA);
+    cart.add(cartItemB);
+    expect(cart.items.length, 2);
+    expect(cartItemA.priceNet, 2000000);
+    expect(cartItemA.vatValue, 200000);
+    expect(cartItemA.importDutyValue, 0);
+    expect(cartItemA.priceGross, 2200000);
+    expect(cartItemB.priceNet, 6000000);
+    expect(cartItemB.vatValue, 300000);
+    expect(cartItemB.importDutyValue, 300000);
+    expect(cartItemB.priceGross, 6600000);
+    expect(cart.priceNet, 8000000);
+    expect(cart.vatValue, 500000);
+    expect(cart.importDutyValue, 300000);
+    expect(cart.priceGross, 8800000);
   });
 }
