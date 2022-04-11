1fe8bca370e70e942445a545aea1d4421d81d244
AUSoftAndreas <andreas_hoefer@cdemy.de>
2022-04-11 12:13:16 +0200

    vat rates implemented

--- lib/entities/vat.dart
+++ lib/entities/vat.dart
@@ -1,5 +1,24 @@
-abstract class Vat {}
+abstract class Vat {
+  int get vat;
+}
 
 class FullVat implements Vat {
   const FullVat();
+
+  @override
+  int get vat => 10;
+}
+
+class ReducedVat implements Vat {
+  const ReducedVat();
+
+  @override
+  int get vat => 5;
+}
+
+class NoVat implements Vat {
+  const NoVat();
+
+  @override
+  int get vat => 0;
 }
--- test/vat_test.dart
+++ test/vat_test.dart
@@ -1,5 +1,4 @@
 import 'package:itemis/entities/vat.dart';
-import 'package:test/scaffolding.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -16,5 +15,20 @@ void main() {
   test('FullVat can be constructred ', () {
     final noVat = NoVat();
     expect(noVat, isA<Vat>());
+  });
+
+  test('FullVat has a VAT of 10 (percent)', () {
+    final fullVat = FullVat();
+    expect(fullVat.vat, 10);
+  });
+
+  test('ReducedVat has a VAT of 5 (percent)', () {
+    final reducedVat = ReducedVat();
+    expect(reducedVat.vat, 5);
+  });
+
+  test('NoVat has a VAT of 0 (percent)', () {
+    final noVat = NoVat();
+    expect(noVat.vat, 0);
   });
 }
